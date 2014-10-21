a313864dc2c97c770eed96c9acf9de2e5b61a77a
beck410 <beck410@hotmail.com>
2014-10-21 14:27:18 -0500

    Added json to pull student data

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
+//Global vars
+var $selectBox = document.querySelector('select');
+var $studentListParent = document.querySelector('.pairing-list');
+var $form = document.getElementById("group-generator");	
+var input = document.querySelector(".people-number");
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
-	
-	var $selectBox = document.querySelector('select')
-	var studentArray = ['sue', 'bob', 'bill', 'beck', 'lucy', 'penny', 'jez', 'dave', 'luke', 'john'];
-	var $studentListParent = document.querySelector('.pairing-list');
- 	var $form = document.getElementById("group-generator");	
-	var input = document.querySelector(".people-number");
 
 	//Event Listeners
 
 	$form.addEventListener('submit', function(event){	
-		event.preventDefault();
-		var $selectBoxValue = $selectBox.options[$selectBox.selectedIndex].value;		
-		var peopleNumber;
-		var shuffledStudents = shuffleStudents(studentArray);
-		var studentsClone = studentArray.clone(); 
-		//removeExistingStudents();
 		
-		$studentListParent.innerText = "";
-		switch($selectBoxValue) {
-			case "studentPairing":
-				showStudentList(shuffledStudents[0]);
-				break;
+		event.preventDefault();
+		
+		getJSON('https://volunteerism-beck410.firebaseio.com/students.json', function(data){
+			studentArray = data
 			
-			case "neighbourPairing":
-				spliceStudentsArray(studentsClone, 2)
-				break;
+			var $selectBoxValue = $selectBox.options[$selectBox.selectedIndex].value;		
+			var peopleNumber;
+			var shuffledStudents = shuffleStudents(studentArray);
+			var studentsClone = studentArray.clone(); 
+			//removeExistingStudents();
+		
+			$studentListParent.innerText = "";
+			switch($selectBoxValue) {
+				case "studentPairing":
+					showStudentList(shuffledStudents[0]);
+					break;
+				
+				case "neighbourPairing":
+					spliceStudentsArray(studentsClone, 2, " and ")
+					break;
 
-			case "randomPairing":
-				spliceStudentsArray(shuffledStudents, 2);		
+				case "randomPairing":
+					spliceStudentsArray(shuffledStudents, 2, " and ");		
+					break;
+
+				case "neighbourThreePairing":
+					spliceStudentsArray(studentsClone, 3, ", ");
+					break;
+
+				case "randomNPairing":
+				var inputValue = document.querySelector(".people-number").value;	
+				spliceStudentsArray(shuffledStudents, inputValue, ", ");
 				break;
-			case "neighbourThreePairing":
-				spliceStudentsArray(studentsClone, 3);
+				
+		  default: 
+					return;
 				break;
-			case "randomNPairing":
-			var inputValue = document.querySelector(".people-number").value;	
-			spliceStudentsArray(shuffledStudents, inputValue);
-			break;
-			default: 
-				return;
-				break;
-		}
+			}
+		});
 	});
 
 	//show people input box if select box value is randomNPairing otherwise hide it
@@ -52,55 +63,68 @@ document.addEventListener('DOMContentLoaded', function(){
 				hideNumberInput();
 			}
 	});
+}); //end of DOMContentLoaded
 
-	//Methods
-	Array.prototype.clone = function(){
-		return this.slice(0);
-	}
+//Methods
+Array.prototype.clone = function(){
+	return this.slice(0);
+}
 	
-	//Basic Functions
+//Basic Functions
+
+//JSON  function to get list
+
+function getJSON(url, cb){
+ xhr = new XMLHttpRequest();
+
+ xhr.open('GET', url);
+  
+ xhr.onload = function(){
+   cb(JSON.parse(xhr.responseText));
+ }
+  
+  xhr.send();
+}
+
+//clones & shuffles original student list
+function shuffleStudents(array){
+	var cloneArray = array.clone(); 
 	
-	//remove children of .pairing-list
+  for(var j,x,i, i=cloneArray.length; i; j=Math.floor(Math.random() * i), x=cloneArray[--i], cloneArray[i] = cloneArray[j], cloneArray[j] = x);
+	
+  return cloneArray;
+}
 
-	//clones & shuffles original student list
-	function shuffleStudents(array){
-		var cloneArray = array.clone(); 
-		for(var j,x,i, i=cloneArray.length; i; j=Math.floor(Math.random() * i), x=cloneArray[--i], cloneArray[i] = cloneArray[j], cloneArray[j] = x);
-		return cloneArray;
-	}
-
-  //creates new li and inserts student names
-	function showStudentList(liText){
+//creates new li and inserts student names
+function showStudentList(liText){
 		
-		//add new li element
-		var li = document.createElement('li');
-		//add text to p element
-		var name = document.createTextNode(liText);
-		//append text to p element
-		li.appendChild(name);
-		//append p to pairing-list div
-		$studentListParent.appendChild(li);
-	}
+  //add new li element
+	var li = document.createElement('li');
+	//add text to p element
+	var name = document.createTextNode(liText);
+	//append text to p element
+	li.appendChild(name);
+	//append p to pairing-list div
+	$studentListParent.appendChild(li);
+}
 
-	//show people-number input
-	function hideNumberInput(){
-  	input.classList.add("hidden");		
-	}
+//show people-number input
+function hideNumberInput(){
+  input.classList.add("hidden");		
+}
 
-	//hide people-number input
-	function showNumberInput(){
-		input.classList.remove("hidden");			
-	}
+//hide people-number input
+function showNumberInput(){
+	input.classList.remove("hidden");			
+}
 
-	//splices student array and sends student names to showStudentList function
-	function spliceStudentsArray(students, endIndex) {
-		while(students.length > 0){
-			var studentNames = students.splice(0, endIndex);
-			var innerText = studentNames.join(" and ");
-		  //call showStudentList(string)
-			showStudentList(innerText);
-		}
+//splices student array and sends student names to showStudentList function
+function spliceStudentsArray(students, endIndex, joiner) {
+	while(students.length > 0){
+		var studentNames = students.splice(0, endIndex);
+		var innerText = studentNames.join(joiner);
+		//call showStudentList(string)
+		showStudentList(innerText);
 	}
+}
 	
-});
-
